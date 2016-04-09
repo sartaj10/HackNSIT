210c2acbcdb2bd71496ee981ecebe7ec7868af26
sadana-ritvik <ritviksadana@gmail.com>
2016-04-09 16:50:31 +0530

    tweets fetched into template

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, json, url_for
+import tweepy
 
 app = Flask(__name__)
 
@@ -6,6 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 def main():
 	return render_template('index.html')
+
+@app.route('/twitter')
+def showtweets():
+    consumer_key        = '0myASUiii6fCxbpTHyXNryP9k'
+    consumer_secret     = 'GYIqctWSh6NvGWexo5QG8gTLWHMGsOi1mui5SuxSGwu7JeH1WZ'
+    access_token        = '1259588774-i76RdUMP8r7F9oPzqLwJwzdvwgmATEXMJkMEVDz'
+    access_token_secret = 'zOyrLduYcEyZvPjuohR4VU0HeGDFSDYLyEfXmLABIJ1ny'
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print tweet.text.encode('utf8')
+        
+    return render_template('tweets.html', tweets = public_tweets)
 
 # App Run
 if __name__ == '__main__':
